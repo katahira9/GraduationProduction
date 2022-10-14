2fc95f672594ef238a2900c12a5a5877aecb633e
katahira9 <potiriku@gmail.com>
2022-10-14 11:18:04 +0900

    inte little

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/fxaa/fxaaGameMode.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FXAA_fxaaGameMode_generated_h
+#error "fxaaGameMode.generated.h already included, missing '#pragma once' in fxaaGameMode.h"
+#endif
+#define FXAA_fxaaGameMode_generated_h
+
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_SPARSE_DATA
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_RPC_WRAPPERS
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAfxaaGameMode(); \
+	friend struct Z_Construct_UClass_AfxaaGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AfxaaGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/fxaa"), FXAA_API) \
+	DECLARE_SERIALIZER(AfxaaGameMode)
+
+
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAfxaaGameMode(); \
+	friend struct Z_Construct_UClass_AfxaaGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AfxaaGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/fxaa"), FXAA_API) \
+	DECLARE_SERIALIZER(AfxaaGameMode)
+
+
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	FXAA_API AfxaaGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AfxaaGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(FXAA_API, AfxaaGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AfxaaGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	FXAA_API AfxaaGameMode(AfxaaGameMode&&); \
+	FXAA_API AfxaaGameMode(const AfxaaGameMode&); \
+public:
+
+
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	FXAA_API AfxaaGameMode(AfxaaGameMode&&); \
+	FXAA_API AfxaaGameMode(const AfxaaGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(FXAA_API, AfxaaGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AfxaaGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AfxaaGameMode)
+
+
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define fxaa_Source_fxaa_fxaaGameMode_h_9_PROLOG
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_SPARSE_DATA \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_RPC_WRAPPERS \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_INCLASS \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define fxaa_Source_fxaa_fxaaGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_SPARSE_DATA \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	fxaa_Source_fxaa_fxaaGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FXAA_API UClass* StaticClass<class AfxaaGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID fxaa_Source_fxaa_fxaaGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Intermediate/Build/Win64/fxaaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30143
+#include "G:/fxaa/Intermediate/Build/Win64/fxaaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
