c88f5dc7a41342092d7392ec68cc6e2dbda4fb9c
ueda_sakuya <uedasakuya96pg@gmail.com>
2023-01-16 00:24:40 +0900

    小さいの

---
+++ Plugins/KM_GAHelperForBP/Intermediate/Build/Win64/UE4Editor/Inc/KM_GAHelperForBP/PostHTTPActor.generated.h
@@ -0,0 +1,101 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef KM_GAHELPERFORBP_PostHTTPActor_generated_h
+#error "PostHTTPActor.generated.h already included, missing '#pragma once' in PostHTTPActor.h"
+#endif
+#define KM_GAHELPERFORBP_PostHTTPActor_generated_h
+
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_SPARSE_DATA
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execMyHttpCall);
+
+
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execMyHttpCall);
+
+
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPostHTTPActor(); \
+	friend struct Z_Construct_UClass_APostHTTPActor_Statics; \
+public: \
+	DECLARE_CLASS(APostHTTPActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/KM_GAHelperForBP"), NO_API) \
+	DECLARE_SERIALIZER(APostHTTPActor)
+
+
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesAPostHTTPActor(); \
+	friend struct Z_Construct_UClass_APostHTTPActor_Statics; \
+public: \
+	DECLARE_CLASS(APostHTTPActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/KM_GAHelperForBP"), NO_API) \
+	DECLARE_SERIALIZER(APostHTTPActor)
+
+
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APostHTTPActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APostHTTPActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APostHTTPActor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APostHTTPActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APostHTTPActor(APostHTTPActor&&); \
+	NO_API APostHTTPActor(const APostHTTPActor&); \
+public:
+
+
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APostHTTPActor(APostHTTPActor&&); \
+	NO_API APostHTTPActor(const APostHTTPActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APostHTTPActor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APostHTTPActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(APostHTTPActor)
+
+
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_PRIVATE_PROPERTY_OFFSET
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_10_PROLOG
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_PRIVATE_PROPERTY_OFFSET \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_SPARSE_DATA \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_RPC_WRAPPERS \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_INCLASS \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_PRIVATE_PROPERTY_OFFSET \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_SPARSE_DATA \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_INCLASS_NO_PURE_DECLS \
+	GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> KM_GAHELPERFORBP_API UClass* StaticClass<class APostHTTPActor>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID GraduationProduction_forclient_Plugins_KM_GAHelperForBP_Source_KM_GAHelperForBP_Public_PostHTTPActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
